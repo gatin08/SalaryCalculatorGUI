6bfe3e11d2565f2a850c5bc51c998c1661d20abc
nailfrompg <nailfrompg@gmail.com>
2019-02-25 20:16:04 +0500

    Создание заглушки класса расчёта (за реализацию отвечает Юрий Истомин)

---
+++ SalaryCalculatorGUI/src/SalaryPackage/SalaryClass.java
@@ -0,0 +1,8 @@
+package SalaryPackage;
+
+public class SalaryClass {
+	public double[] Calculate(double input) {
+		//Заглушка: за реализацию класса расчёта отвечает Юрий Истомин 
+		return new double[] { 0, 0, 0 };
+	}
+}
